d0b053aaf67e6e581a7687f514b0f811c31e4e40
Pedro Moreira <pedrogonmoreira@gmail.com>
2024-07-04 00:00:47 -0300

    criação completa das rotas

--- server.js
+++ server.js
@@ -1,25 +1,66 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const users = [];
-
 app.post('/usuarios', async (req, res) => {
-  prisma.user.create({
-    data: {
-      name: req.body.name,
-      age: req.body.age,
-      email: req.body.email
-    },
-  });
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.put('/usuarios/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.emai,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
 });
 
 app.get('/usuarios', async (req, res) => {
-  res.json(users);
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
 });
 
-app.listen(3000);
+app.delete('/usuarios/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
