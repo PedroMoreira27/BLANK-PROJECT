0dce1cd398117a175229170dc404b78c7360f20d
Pedro Moreira <pedrogonmoreira@gmail.com>
2024-07-06 22:59:55 -0300

    ajuste das rotas gerais do backend

--- server.js
+++ server.js
@@ -48,7 +48,252 @@ app.get('/usuarios', async (req, res) => {
   }
 });
 
+app.get('/usuarios', async (req, res) => {
+  let users = [];
+
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.query.age,
+      },
+    });
+  } else {
+    users = await prisma.user.findMany({});
+  }
+});
+
 app.delete('/usuarios/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
+
+
+
+app.post('/anime', async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.put('/anime/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.emai,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.get('/anime', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+});
+
+app.get('/anime', async (req, res) => {
+  let users = [];
+
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.query.age,
+      },
+    });
+  } else {
+    users = await prisma.user.findMany({});
+  }
+});
+
+app.delete('/anime/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
+
+
+app.post('/filme', async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.put('/filme/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.emai,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.get('/filme', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+});
+
+app.get('/filme', async (req, res) => {
+  let users = [];
+
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.query.age,
+      },
+    });
+  } else {
+    users = await prisma.user.findMany({});
+  }
+});
+
+app.delete('/filme/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
+
+
+app.post('/manga', async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.put('/manga/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.emai,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.get('/manga', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+});
+
+app.get('/manga', async (req, res) => {
+  let users = [];
+
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.query.age,
+      },
+    });
+  } else {
+    users = await prisma.user.findMany({});
+  }
+});
+
+app.delete('/manga/:id', async (req, res) => {
   try {
     const user = await prisma.user.delete({
       where: {
